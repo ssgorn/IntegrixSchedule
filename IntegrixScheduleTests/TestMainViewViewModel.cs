11dae36fceea09405b0ef6001f54347acd4dbe3b
ssgorn <ssgorn@rambler.ru>
2017-02-13 17:40:40 +0300

    Add files via upload

---
+++ IntegrixScheduleTests/TestMainViewViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using IntegrixSchedule.BisnessObjects;
+using IntegrixSchedule.Model;
+using IntegrixSchedule.Test;
+using IntegrixSchedule.View;
+using IntegrixSchedule.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrixScheduleTests
+{
+	[TestClass]
+	public class TestMainViewViewModel
+	{
+		private static Guid _idOrganization = Guid.NewGuid();
+
+		[TestMethod]
+		public void CreateScheduleTest()
+		{
+			var mmmv = new MainViewViewModel(new MainView());
+			var ds = new DateTime(2017, 2, 1);
+			var de = new DateTime(2017, 2, 10);
+
+			DBManager.Instance = new MoqManager();
+
+			var res = ((IMainViewModelCalc) mmmv).CreateSchedule(ds, de, _idOrganization);
+			Assert.IsTrue(res.Value.Min(x => x.TimeStart).Date == res.Key.DateStart.Date);
+		}
+
+		private class MoqManager : IDBManager
+		{
+			public ScheduleTemplate GetActualScheduleTemplate(Guid idOrganization)
+			{
+				return new ScheduleTemplate()
+				{
+					Id = Guid.NewGuid(), IsActual = true, IdOrganization = _idOrganization, TimeStart = new DateTime(2000, 1, 1, 10, 0, 0),
+					TimeEnd = new DateTime(2000, 1, 1, 14, 0, 0), RecipietTime = 30, BreakeStartTime = new DateTime(2000, 1, 1, 11, 0, 0),
+					BreakeEndTime = new DateTime(2000, 1, 1, 11, 35, 0), IsWork1 = true, IsWork2 = true, IsWork3 = true, IsWork4 = true,
+					IsWork5 = true, IsWork6 = true
+				};
+			}
+
+			public void SetNewDBContext(IMainDBContext context)
+			{
+				throw new NotImplementedException();
+			}
+
+			public ObservableCollection<Organization> OrganizationsGet(bool force = false)
+			{
+				throw new NotImplementedException();
+			}
+
+			public ObservableCollection<ScheduleHeader> ScheduleHeadersGet(Guid? idOrganization)
+			{
+				throw new NotImplementedException();
+			}
+
+			public ScheduleDetailsCollection ScheduleDetailsGet(Guid? idHeader)
+			{
+				throw new NotImplementedException();
+			}
+
+			public void SaveScheduleHeader(ScheduleHeader hed)
+			{
+				throw new NotImplementedException();
+			}
+
+			public void SaveScheduleDetails(List<ScheduleDetail> dets)
+			{
+				throw new NotImplementedException();
+			}
+
+			public ObservableCollection<ScheduleTemplate> SceduleTemplatesGet(Guid idOrganization)
+			{
+				throw new NotImplementedException();
+			}
+
+			public ScheduleTemplate DeleteSelectedTemplate(ScheduleTemplate templ)
+			{
+				throw new NotImplementedException();
+			}
+
+			public void SaveSelectedTemplate(ScheduleTemplate templ)
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
---
+++ IntegrixScheduleTests/TestScheduleDetailsCollection.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntegrixSchedule.BisnessObjects;
+using IntegrixSchedule.Model;
+
+namespace IntegrixScheduleTests
+{
+	[TestClass]
+	public class TestScheduleDetailsCollection
+	{
+		[TestMethod]
+		public void TestEmptyConstructor()
+		{
+			var col = new ScheduleDetailsCollection();
+			Assert.IsTrue(col.Count == 0);
+		}
+
+		[TestMethod]
+		public void TestConstructorEmptyList()
+		{
+			var col = new ScheduleDetailsCollection(new List<ScheduleDetail>(), Guid.Empty);
+			Assert.IsTrue(col.Count == 0);
+		}
+
+		[TestMethod]
+		public void TestConstructorCorrectList()
+		{
+			var list = CreateCurrectMoqList();
+
+			var col = new ScheduleDetailsCollection(list, list[0].IdScheduleHeader);
+			Assert.IsTrue(col.Count == 3);
+			Assert.IsTrue(col[0].ScheduleDetails.Count == 4);
+			Assert.IsTrue(col[1].ScheduleDetails.Count == 3);
+			Assert.IsTrue(col[2].ScheduleDetails.Count == 3);
+
+			Assert.IsTrue(col[0].IdScheduleHeader == list[0].IdScheduleHeader);
+		}
+
+		private List<ScheduleDetail> CreateCurrectMoqList()
+		{
+			var res = new List<ScheduleDetail>();
+
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 1, 8, 0, 0),
+				TimeEnd = new DateTime(2000, 1, 1, 8, 15, 0),
+			});
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 1, 8, 15, 0),
+				TimeEnd = new DateTime(2000, 1, 1, 8, 30, 0),
+			});
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 1, 8, 30, 0),
+				TimeEnd = new DateTime(2000, 1, 1, 8, 45, 0),
+			});
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 1, 8, 45, 0),
+				TimeEnd = new DateTime(2000, 1, 1, 9, 0, 0),
+			});
+
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 2, 8, 0, 0),
+				TimeEnd = new DateTime(2000, 1, 2, 8, 15, 0),
+			});
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 2, 8, 15, 0),
+				TimeEnd = new DateTime(2000, 1, 2, 8, 30, 0),
+			});
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 2, 8, 30, 0),
+				TimeEnd = new DateTime(2000, 1, 2, 8, 45, 0),
+			});
+
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 4, 8, 0, 0),
+				TimeEnd = new DateTime(2000, 1, 4, 8, 15, 0),
+			});
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 4, 8, 15, 0),
+				TimeEnd = new DateTime(2000, 1, 4, 8, 45, 0),
+				IsBreak = true
+			});
+			res.Add(new ScheduleDetail()
+			{
+				Id = Guid.NewGuid(),
+				TimeStart = new DateTime(2000, 1, 4, 8, 45, 0),
+				TimeEnd = new DateTime(2000, 1, 4, 9, 0, 0),
+			});
+
+			return res;
+		}
+	}
+}
